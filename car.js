23c32c08526c0307061aec07e163931a5f5cebb1
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-07 16:16:12 +0700

    add hasOwnProperty() to car and change the input type for house class into an object

--- car.js
+++ car.js
@@ -3,7 +3,7 @@
 class Car {
 
   start_car(options) {
-    if (options && options['headlights']) {
+    if (options && options.hasOwnProperty('headlights')) {
       this.headlights = options['headlights']
     } else {
       this.headlights = true
--- house.js
+++ house.js
@@ -2,16 +2,16 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(objOfHouse) {
+    this.address = objOfHouse.address;
+    this.square_feet = objOfHouse.square_feet;
+    this.num_bedrooms = objOfHouse.num_bedrooms;
+    this.num_baths = objOfHouse.num_baths;
+    this.cost = objOfHouse.cost;
+    this.down_payment = objOfHouse.down_payment;
+    this.sold = objOfHouse.sold;
+    this.short_sale = objOfHouse.short_sale;
+    this.has_tenants = objOfHouse.has_tenants;
   }
 
   obscure_address() {
@@ -33,6 +33,18 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let objHouse = {
+  address: 'address',
+  square_feet: 100,
+  num_bedrooms: 2,
+  num_baths: 2,
+  cost: 12345,
+  down_payment: 12345,
+  sold: true,
+  short_sale: true,
+  has_tenants: true,
+}
+
+const cool = new House(objHouse)
 
 console.log(cool.to_s())
